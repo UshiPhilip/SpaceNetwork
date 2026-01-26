e0089ee0977c7b43d11180909ee0bfd776ebb054
Ushi Philip <ushi7670741@gmail.com>
2026-01-26 14:53:36 +0200

    Created a proxy
    To send messages between far satellites

--- classes.py
+++ classes.py
@@ -5,10 +5,33 @@ import time
 class Satellite(SpaceEntity):
 
     def receive_signal(self, packet: Packet):
-        print(f"[{self.name}] Received: {packet}.")
+        # Chacking if it's an instance
+        if isinstance(packet, RelayPacket):
+            # Unwrapping the message
+            inner_packet = packet.data
+            print(f"[{self.name}] - Unwrapping and forwarding to {inner_packet.receiver}.")
+            # Sending to the receiver
+            transmission_attempt(inner_packet)
+        else:
+            print(f"Final destination reached: {packet.data}")
+
+
+class RelayPacket(Packet):
+    def __init__(self, packet_to_relay, sender, proxy):
+        super().__init__(data=packet_to_relay, sender=sender, receiver=proxy)
+        self.sender = sender
+
+    def __repr__(self):
+        return f"RelayPacket (Relaying [{self.data}] to {self.receiver} from {self.sender})"
+
+
+class NotSatellite(SpaceEntity):
+    def receive_signal(self, packet: Packet):
+        pass
+
 
 def transmission_attempt(packet):
-    spaceship = SpaceNetwork(level=3)
+    spaceship = SpaceNetwork(level=4)
     while True:
         try:
             spaceship.send(packet)
@@ -21,9 +44,9 @@ def transmission_attempt(packet):
             time.sleep(1)
         except DataCorruptedError:
             print("Data corrupted, retrying...")
-            raise "BrokenConnectionError"
         except LinkTerminatedError:
             print("Link lost")
+            raise "BrokenConnectionError"
         except OutOfRangeError:
             print("Target out of range.")
             raise "BrokenConnectionError"
